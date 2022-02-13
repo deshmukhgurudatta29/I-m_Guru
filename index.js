a068849669d5aa3c76c6bfb36925872f66217910
deshmukhgurudatta29 <80634882+deshmukhgurudatta29@users.noreply.github.com>
2022-02-13 13:42:10 +0530

    Initial_CV_Website_Files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+alert("Hello Duniya walo")
+var dec = prompt("Type 1 for my Info")
+if(dec = 1){
+    window.open("https://deshmukhgurudatta29.github.io/gurudatta-personel-site/")
+} 
+else{
+    alert("ok")
+}
